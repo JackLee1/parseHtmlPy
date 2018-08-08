f36e46fb9a8a2ebcac3ac4c601ff7d0843f87053
小二郎 <wang@xiaoerlangdeMacBook-Pro.local>
2018-08-08 18:00:37 +0800

    添加Python文件

--- parseHtml.py
+++ parseHtml.py
@@ -11,18 +11,64 @@ kBaseUrl = 'http://baidu.com'
 
 # http://www.runoob.com/python3
 
+
+#解析播放页面
+def parsePlayView(videoType,movieName,url):
+
+    # 加载子菜单内容列表下内容详情
+    detailReq = requests.get(kBaseUrl + url)
+    detailData = detailReq.text.encode(detailReq.encoding).decode('utf-8')
+    # print(detailData)
+    detailSoup = bs4.BeautifulSoup(detailData, 'html.parser')
+    # playData = detailSoup.find('div', class_='left1')
+
+    # 获取视频源播放地址
+    hlsurl = detailSoup.find(text=re.compile('var vHLSurl'))
+
+    # print(hlsurl)
+
+    # 解析hlsurl
+
+    hlsEle = (hlsurl.replace(";", "")).split("=")
+
+    lastEle = hlsEle[len(hlsEle) - 1]
+    # print(lastEle)
+
+    newArr = lastEle.split('+')
+
+    newArrLastObj = ((newArr[len(newArr) - 1]).split('\r\n'))[0]
+    # print(newArrLastObj)
+
+    # 获取js代码 随机选择的host
+    jsReq = requests.get(kBaseUrl + '/js/sp.js')
+    jsData = jsReq.text.encode(resp.encoding).decode('utf-8')
+    # print(jsData)
+
+    # 检查host在js中的下标
+    index = jsData.find(newArr[1], 0, len(jsData))
+    # print('index = %s' % index)
+    indexStr = jsData[index - 40:index - 5]
+    # print(indexStr)
+    if indexStr.__contains__('"'):
+        # 获取下标
+        lIndex = indexStr.find('"')
+        rIndex = indexStr.rfind('"')
+        # print(lIndex, rIndex)
+        # 截取host地址
+        hostCon = indexStr[lIndex + 1:rIndex] + ':8011'
+        # print(hostCon)
+
+        totalUrl = newArr[0] + hostCon + newArrLastObj
+        totalUrl = totalUrl.replace('"', '')
+        print(videoType + '/' + movieName + totalUrl)
+
+        # conList.append(movieName + totalUrl + '\n')
+
+        return movieName + totalUrl + '\n'
+
+
 # 解析二级页面视频播放地址
 def parsePlayData(videoType,url):
-
-
-    path = '/Users/wang/Desktop/data/' + videoType
-
-    if not os.path.exists(path):
-        # 不存在文件
-        os.mkdir(path)
-    fullpath = '%s/%s.txt' % (path, videoType)
-
-    f = open(fullpath,'w')
 
     # 获取子菜单HTML
     req = requests.get(kBaseUrl + url)
@@ -38,64 +84,22 @@ def parsePlayData(videoType,url):
     tt = contentSoup.find('div', class_='content bord mtop').find('ul').find_all('li')
     print('-----------------------content--------------------------')
 
+    conList = []
+
     # 遍历菜单list
+
     for t in tt:
         # 获取子菜单第一个元素内容
         # t = tt[0]
         # print(t.text)
         # print(t.a['href'])
 
-        # 加载子菜单内容列表下内容详情
-        detailReq = requests.get(kBaseUrl + t.a['href'])
-        detailData = detailReq.text.encode(resp.encoding).decode('utf-8')
-        # print(detailData)
-        detailSoup = bs4.BeautifulSoup(detailData, 'html.parser')
-        # playData = detailSoup.find('div', class_='left1')
+        content = parsePlayView(videoType,t.text,t.a['href'])
+        conList.append(content)
+        # f.write(content)
 
-        # 获取视频源播放地址
-        hlsurl = detailSoup.find(text=re.compile('var vHLSurl'))
-
-        # print(hlsurl)
-
-        # 解析hlsurl
-
-        hlsEle = (hlsurl.replace(";", "")).split("=")
-
-        lastEle = hlsEle[len(hlsEle) - 1]
-        # print(lastEle)
-
-        newArr = lastEle.split('+')
-
-        newArrLastObj = ((newArr[len(newArr) - 1]).split('\r\n'))[0]
-        # print(newArrLastObj)
-
-        # 获取js代码 随机选择的host
-        jsReq = requests.get(kBaseUrl + '/js/sp.js')
-        jsData = jsReq.text.encode(resp.encoding).decode('utf-8')
-        # print(jsData)
-
-        # 检查host在js中的下标
-        index = jsData.find(newArr[1], 0, len(jsData))
-        # print('index = %s' % index)
-        indexStr = jsData[index - 40:index - 5]
-        # print(indexStr)
-        if indexStr.__contains__('"'):
-            # 获取下标
-            lIndex = indexStr.find('"')
-            rIndex = indexStr.rfind('"')
-            # print(lIndex, rIndex)
-            # 截取host地址
-            hostCon = indexStr[lIndex + 1:rIndex] + ':8011'
-            # print(hostCon)
-
-            totalUrl = newArr[0] + hostCon + newArrLastObj
-            totalUrl = totalUrl.replace('"', '')
-            print(videoType + '/' + t.text + totalUrl)
-
-            # 写入数据
-            f.write(t.text + totalUrl + '\n')
-
-    f.close()
+    # f.close()
+    writeDataToFile(videoType,conList)
 
 # 获取首页菜单数据
 def getHomeMenu(menu):
@@ -105,7 +109,6 @@ def getHomeMenu(menu):
     print(len(menu))
 
     for div in menu:
-
 
         values = ''.join(str(v) for v in div)
 
@@ -123,9 +126,11 @@ def getHomeMenu(menu):
                 else:
 
                     # 菜单下的内容
-                    content = '%s : %s%s' % (soup2.contents[i].string, url, soup2.contents[i].a['href'])
+                    content = '%s : %s%s' % (soup2.contents[i].string, kBaseUrl, soup2.contents[i].a['href'])
 
                     if soup2.contents[1].string == '视频区':
+
+                        print(soup2.contents[i].string + soup2.contents[i].a['href'])
                         parsePlayData(soup2.contents[i].string, soup2.contents[i].a['href'])
 
                         print('-----------------------content--------------------------')
@@ -136,7 +141,7 @@ def getHomeMenu(menu):
                 title = '%s' % soup2.contents[i].string
                 menuList.append(title)
 
-    writeDataToFile('/data', menuList)
+    writeDataToFile('menu', menuList)
 
 
 
@@ -145,20 +150,20 @@ def getHomeMenu(menu):
 # 写入数据
 def writeDataToFile(fileName, data):
 
-    path = '/Users/wang/Desktop' + fileName
+    path = '/Users/wang/Desktop/data/' + fileName
 
     if not os.path.exists(path):
         # 不存在文件
-        os.mkdir(path)
-    fullpath = '%s%s.txt' % (path, fileName)
+        os.makedirs(path)
+    fullpath = '%s/%s.txt' % (path, fileName)
 
     if os.path.exists(fullpath):
         return
     print(fullpath)
+
     f = open(fullpath, 'w')
 
-    for str in data:
-        f.write(str)
+    f.writelines(data)
 
     f.close()
 
